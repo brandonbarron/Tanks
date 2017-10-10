73d10af7993bb5addccc2097feb2a079379fb38a
brandonbarron <brandonbarron38@gmail.com>
2017-10-09 20:25:20 -0600

    Inital project creation

---
+++ TanksServer/TanksServer/MainViewModel.cs
@@ -0,0 +1,75 @@
+﻿namespace TanksServer
+{
+    public class MainViewModel : System.ComponentModel.INotifyPropertyChanged
+    {
+        private readonly MahApps.Metro.Controls.Dialogs.IDialogCoordinator dialogCoordinator;
+
+        public MainViewModel(MahApps.Metro.Controls.Dialogs.IDialogCoordinator instance)
+        {
+            this.dialogCoordinator = instance;
+            _startServerCommand = new DelegateCommand<object>((p) => StartServer());
+            _stopServerCommand = new DelegateCommand<object>((p) => StopServer());
+            ServerStatus = "Dead";
+            CurrentGames = new System.Collections.ObjectModel.ObservableCollection<object>()
+            {
+                new
+                {
+                    Id = 1,
+                    Players = 3,
+                    TotalTime = "1:03",
+                    Winner = "Player1"
+                },
+                new
+                {
+                    Id = 1,
+                    Players = 3,
+                    TotalTime = "1:03",
+                    Winner = "Player1"
+                },
+                new
+                {
+                    Id = 1,
+                    Players = 3,
+                    TotalTime = "1:03",
+                    Winner = "Player1"
+                }
+            };
+        }
+
+        private readonly DelegateCommand<object> _startServerCommand;
+        public DelegateCommand<object> StartServerCommand { get => _startServerCommand; }
+        private readonly DelegateCommand<object> _stopServerCommand;
+        public DelegateCommand<object> StopServerCommand { get => _stopServerCommand; }
+
+        private string _serverStatus;
+        public string ServerStatus
+        {
+            get => _serverStatus;
+            private set
+            {
+                _serverStatus = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public System.Collections.ObjectModel.ObservableCollection<object> CurrentGames { get; private set; }
+
+
+        private void StartServer()
+        {
+
+        }
+
+        private void StopServer()
+        {
+
+        }
+
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ TanksServer/TanksServer/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TanksServer
+{
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
+    {
+        MainViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainViewModel(MahApps.Metro.Controls.Dialogs.DialogCoordinator.Instance);
+            this.DataContext = vm;
+        }
+    }
+}
